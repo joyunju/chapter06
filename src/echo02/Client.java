92750915d83e32021c4ab50fa9290af36ff09d16
joyunju <whdbswn1113@naver.com>
2022-05-11 10:55:10 +0900

    InputStream in = System.in; // 키보드 연결

---
+++ src/echo02/Client.java
@@ -0,0 +1,82 @@
+// window
+
+package echo02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+
+		Socket socket = new Socket();
+
+		System.out.println("<클라이언트 시작>");
+		System.out.println("==============================================");
+
+		System.out.println("[서버에 연결을 요청합니다.]");
+		socket.connect(new InetSocketAddress("192.168.25.18", 10001));
+
+		System.out.println("[서버에 연결 되었습니다.]");
+
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 스캐너
+		// Scanner sc = new Scanner(System.in); 같은 역할 ↓
+		InputStream in = System.in; // 키보드 연결
+		InputStreamReader sisr = new InputStreamReader(in, "UTF-8");
+		BufferedReader sbr = new BufferedReader(sisr);
+
+		while (true) {
+			// 키보드입력
+			// String str = sc.nextLine();
+			String str = sbr.readLine();
+
+			if (str.equals("/q")) {
+				break;
+			}
+
+			// 메세지 보내기
+			bw.write(str);
+			bw.newLine();
+			bw.flush();
+
+			// 메세지 받기
+			String reMsg = br.readLine();
+			System.out.println("server:[" + reMsg + "]");
+
+		}
+
+		System.out.println("==============================================");
+		// System.out.println("<클라이언트 종료>");
+		OutputStream out = System.out;
+		OutputStreamWriter posw = new OutputStreamWriter(out, "UTF-8");
+		BufferedWriter pbw = new BufferedWriter(posw);
+		// 메세지 보내기
+		pbw.write("클라이언트 종료");
+		pbw.newLine();
+		pbw.flush();
+
+		// sc.close();
+		br.close();
+		bw.close();
+		socket.close();
+
+	}
+
+}
---
+++ src/echo02/Server.java
@@ -0,0 +1,70 @@
+//window
+
+package echo02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+
+		// window ip 주소 : 터미널을 열고 ipconfig 명령어를 실행
+
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("192.168.25.18", 10001));
+
+		System.out.println("<서버시작>");
+		System.out.println("==========================================================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+
+		Socket socket = serverSocket.accept();
+		System.out.println("[클라이언트가 연결 되었습니다.]");
+
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		//
+		while (true) {
+			// 메세지받기
+			String msg = br.readLine();
+
+			if (msg == null) {
+				break;
+			}
+
+			System.out.println("받은메세지:" + msg);
+
+			// 메세지 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+
+		}
+
+		System.out.println("==========================================================");
+		System.out.println("<서버종료>");
+
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
+
+	}
+
+}
